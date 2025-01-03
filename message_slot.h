a8131c37c92a2140e18d806d254392e16b3fc85b
assafyaron <128134935+assafyaron@users.noreply.github.com>
2025-01-03 17:04:43 +0200

    Update message_slot.h
    Moved structs into header file

--- message_slot.h
+++ message_slot.h
@@ -10,4 +10,25 @@
 // The ioctl command to set the channel id
 #define MSG_SLOT_CHANNEL _IOW(MAJOR_NUM, 0, unsigned long)
 
+/* =========== Structs =========== */
+
+struct message_slot {
+    /*
+    Struct to describe individual message slots (device files with different minor numbers)
+    Contains a list of channels in the message slot
+    */
+    unsigned long channel_id;
+    struct channel* next;
+    struct channel* tail;
+}; 
+
+struct channel {
+    /*
+    Struct to describe individual channels in a message slot
+    */
+    char* message;
+    unsigned long channel_id;
+    struct channel* next;
+};
+
 #endif // _MESSAGE_SLOT_H_
